4d8a70546a86b267096fac41e1031e3bdebf6b6b
Chandresh Singh <54761454+The-Pascal@users.noreply.github.com>
2023-10-17 14:01:39 +0530

    Update react-native.config.js | enable ios autolinking

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,6 @@ module.exports = {
     dependency: {
       platforms: {
         android: null,
-        ios: null,
         // add more platform to disable auto-linking for them too
       },
     },
